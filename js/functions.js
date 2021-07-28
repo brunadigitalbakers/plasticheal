11a849053338708c5a40ccb66cf54e6615a0f211
marcelsafont <marcel.safont1983@gmail.com>
2021-07-28 22:27:52 +0200

    about styles

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,20 @@ $( '.swipebox' ).swipebox();
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 4,
-  slidesToScroll: 4
+  slidesToScroll: 4,
 });
+
+$('.paragraph-list-desplegable .list li').each(function(i, ul){
+  console.log(i);
+  if(i > 3){
+    $(this).addClass('hide');
+  }
+})
+
+$('.show-more-list').click(function(e){
+  e.preventDefault();
+  $(this).hide();
+  $('.paragraph-list-desplegable .list li').each(function(i, ul){
+    $(this).removeClass('hide');
+  })
+})
